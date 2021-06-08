6cddedfc070bece3e736489e8bcb7abbbaa43716
Hervé <h.richard.codeur.onlinem>
2021-06-08 11:56:26 +0200

    commit

--- includes/header.php
+++ includes/header.php
@@ -9,52 +9,73 @@
 </head>
 <body>
 
-<div class="header">
-
-<button id="game_category">Games</button>
 
 
-
-<button id="history_category">History</button>
-
-
-<button id="items_category">Items</button>
-
-
-
-
-<button id="character_category">Characters</button>
-
-
-
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="header">
+        <h2>Menu</h2>
+      </div>
+      <div class="row">
+        <div class="column">
+          <h3>Characters</h3>
+          <a href="">Chris Redfield</a>
+          <a href="">Jill Valentine</a>
+          <a href="">Barry Burton</a>
+          <a href="">Albert Wesker</a>
+        </div>
+        <div class="column">
+          <h3>Items</h3>
+          <a href="">Armes</a>
+          <a href="">Soins</a>
+          <a href="">Objets</a>
+          <a href="">Objets clés</a>
+        </div>
+        <div class="column">
+          <h3>Scénario</h3>
+          <a href="">RESIDENT EVIL 1</a>
+          <a href="">RESIDENT EVIL 2</a>
+          <a href="">RESIDENT EVIL 3</a>
+          <a href="">RESIDENT EVIL 4</a>
+        </div>
+        <div class="column">
+          <h3>Mode</h3>
+          <a href="">Mercenaries</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-<div id="characters_sub_category">
-    <a href="">Chris Redfield</a>
-    <a href="">Jill Valentine</a>
-    <a href="">Barry Burton</a>
-    <a href="">Albert Wesker</a>
-</div>
 
-<div id="items_sub_category">
-    <a href="">Armes</a>
-    <a href="">Soins</a>
-    <a href="">Objets</a>
-    <a href="">Objets clés</a>
-</div>
 
-<div id="history_sub_category">
-    <a href="">RESIDENT EVIL 1</a>
-    <a href="">RESIDENT EVIL 2</a>
-    <a href="">RESIDENT EVIL 3</a>
-    <a href="">RESIDENT EVIL 4</a>
-</div>
 
-<div id="game_sub_category">
-    <a href="">RESIDENT EVIL 1</a>
-    <a href="">RESIDENT EVIL 2</a>
-    <a href="">RESIDENT EVIL 3</a>
-    <a href="">RESIDENT EVIL 4</a>
-    <a href="">RESIDENT EVIL 5</a>
-    <a href="">RESIDENT EVIL 6</a>
-</div>
-</div>
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
